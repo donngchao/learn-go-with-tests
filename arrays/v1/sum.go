94a1c0ac2d74bc80f876ba0945ad3fda4d4f0c06
fighting <1559273720@qq.com>
2019-11-14 22:32:29 +0800

    update array sum

--- arrays/v1/sum.go
+++ arrays/v1/sum.go
@@ -8,3 +8,11 @@ func Sum(numbers [5]int) int {
 	}
 	return sum
 }
+
+func Sum1(numbers [6]int) int {
+	sum := 0
+	for i:= 0;i < 6 ;i++{
+		sum += numbers[i]
+	}
+	return sum
+}
--- arrays/v1/sum_test.go
+++ arrays/v1/sum_test.go
@@ -12,4 +12,11 @@ func TestSum(t *testing.T) {
 	if want != got {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
 	}
+
+	numbers1 := [6]int{1,2,3,4,5,6}
+	got1 := Sum1(numbers1)
+	want1 := 21
+	if want != got{
+		t.Errorf("got %d want %d,%v",got1,want1,numbers1)
+	}
 }
