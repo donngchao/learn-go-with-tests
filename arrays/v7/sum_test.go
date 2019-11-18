5346d1f6b33e2091511e96432eeba3d48361685c
fighting <1559273720@qq.com>
2019-11-18 23:32:45 +0800

    slice test

--- arrays/v7/sum_test.go
+++ arrays/v7/sum_test.go
@@ -21,6 +21,22 @@ func TestSum(t *testing.T) {
 
 }
 
+func TestSum2(t *testing.T) {
+
+	t.Run("sum of slice", func(t *testing.T) {
+
+		numbers := []int{1, 2, 3, 4, 5, 6, 7}
+
+		got := Sum(numbers)
+		want := 28
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+}
+
 func TestSumAllTails(t *testing.T) {
 
 	checkSums := func(t *testing.T, got, want []int) {
@@ -41,4 +57,10 @@ func TestSumAllTails(t *testing.T) {
 		checkSums(t, got, want)
 	})
 
+
+	t.Run("sum slices demo1", func(t *testing.T) {
+		got := SumAllTails([]int{1,7,7},[]int{4,5,6},[]int{}, []int{3, 4, 5})
+		want := []int{14,11,0,9}
+		checkSums(t, got, want)
+	})
 }
